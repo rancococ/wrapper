1d5959fe943bc454935463a1bd50ff07a2df30ec
rancococ@qq.com <rancococ@qq.com>
2019-10-23 11:55:11 +0800

    升级apr、ssl版本

--- bin/wrapper-create-linkfile.sh
+++ bin/wrapper-create-linkfile.sh
@@ -59,12 +59,12 @@ cd "${base_dir}"
 # file array
 #
 file_array=(
-libapr-1.so.0.6.5:libapr-1.so,libapr-1.so.0,libapr-1.so.0.6
+libapr-1.so.0.7.0:libapr-1.so,libapr-1.so.0,libapr-1.so.0.7
 libaprutil-1.so.0.6.1:libaprutil-1.so,libaprutil-1.so.0,libaprutil-1.so.0.6
-libcrypto.so.1.0.0:libcrypto.so,libcrypto.so.1,libcrypto.so.1.0
+libcrypto.so.1.1:libcrypto.so,libcrypto.so.1
 libexpat.so.1.6.0:libexpat.so,libexpat.so.1,libexpat.so.1.6
-libssl.so.1.0.0:libssl.so,libssl.so.1,libssl.so.1.0
-libtcnative-1.so.0.2.21:libtcnative-1.so,libtcnative-1.so.0,libtcnative-1.so.0.2
+libssl.so.1.1:libssl.so,libssl.so.1
+libtcnative-1.so.0.2.23:libtcnative-1.so,libtcnative-1.so.0,libtcnative-1.so.0.2
 libz.so.1.2.11:libz.so,libz.so.1,libz.so.1.2
 )
 
